eec3ef591b4105e1bc8be1e578d6d853e1b6c7a7
melonysmith <smith.melonya@gmail.com>
2017-05-04 09:16:26 -0500

    Lint, Comments, Clean Up

--- js/app.js
+++ js/app.js
@@ -1,52 +1,37 @@
-'use strict';
-
-/*
- * A Design by GraphBerry
- * Author: GraphBerry
- * Author URL: http://graphberry.com
- * License: http://graphberry.com/pages/license
- */
-
- // Open offsite navigation.
- $('#nav-expander').on('click', function(e) {
-    e.preventDefault();
-    $('nav').toggleClass('nav-expanded');
+// open nav
+$('#nav-expander').on('click', (e) => {
+  e.preventDefault();
+  $('nav').toggleClass('nav-expanded');
 });
 
-// Close offsite navigation.
- $('.menu .close').on('click', function(e) {
-    e.preventDefault();
-    $('nav').toggleClass('nav-expanded');
+// close nav
+$('.menu .close').on('click', (e) => {
+  e.preventDefault();
+  $('nav').toggleClass('nav-expanded');
 });
 
-// Close offsite navigation after user click on an link in navigation.
-$('.menu  a').on('click', function(e) {
-    e.preventDefault();
-    $('nav').removeClass('nav-expanded');
+// close nav after item clicked
+$('.menu  a').on('click', (e) => {
+  e.preventDefault();
+  $('nav').removeClass('nav-expanded');
 });
 
-
-//Calculate full with of jumbotron.
- function homeFullScreen() {
-
-    var homeSection = $('.home');
-    var windowHeight = $(window).outerHeight();
-
-    if (homeSection.hasClass('home-fullscreen')) {
-
-        $('.home-fullscreen').css('height', windowHeight);
-    }
+// calculate jumotron width
+homeFullScreen() => {
+  const homeSection = $('.home');
+  const windowHeight = $(window).outerHeight();
+  if (homeSection.hasClass('home-fullscreen')) {
+    $('.home-fullscreen').css('height', windowHeight);
+  }
 }
 
-//Initialization
-$(window).load(function () {
-    homeFullScreen();
-
-    smoothScroll.init();
+// initialize
+$(window).load(() => {
+  homeFullScreen();
+  smoothScroll.init();
 });
 
-
-//What happen on window resize
-$(window).resize(function () {
-    homeFullScreen();
+// on window resize...
+$(window).resize(() => {
+  homeFullScreen();
 });
