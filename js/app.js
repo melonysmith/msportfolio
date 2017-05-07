f97d21dd4f1434be5e64597243f667fa7152cb61
Melony Smith <smith.melonya@gmail.com>
2017-05-07 07:52:58 -0500

    Menu Fix
    ?

--- js/app.js
+++ js/app.js
@@ -1,44 +1,37 @@
-'use strict';
-
-/* eslint no-undef: 0 */
-
- /* open nav */
- $('#nav-expander').on('click', function(e) {
-    e.preventDefault();
-    $('nav').toggleClass('nav-expanded');
+// open nav
+$('#nav-expander').on('click', (e) => {
+  e.preventDefault();
+  $('nav').toggleClass('nav-expanded');
 });
 
-/* close nav */
- $('.menu .close').on('click', function(e) {
-    e.preventDefault();
-    $('nav').toggleClass('nav-expanded');
+// close nav
+$('.menu .close').on('click', (e) => {
+  e.preventDefault();
+  $('nav').toggleClass('nav-expanded');
 });
 
-/* close nav after menu link clickd */
-$('.menu  a').on('click', function(e) {
-    e.preventDefault();
-    $('nav').removeClass('nav-expanded');
+// close nav after item clicked
+$('.menu  a').on('click', (e) => {
+  e.preventDefault();
+  $('nav').removeClass('nav-expanded');
 });
 
-/* calculate jumbotron width */
- function homeFullScreen() {
-
-    var homeSection = $('.home');
-    var windowHeight = $(window).outerHeight();
-
-    if (homeSection.hasClass('home-fullscreen')) {
-
-        $('.home-fullscreen').css('height', windowHeight);
-    }
+// calculate jumotron width
+homeFullScreen() => {
+  const homeSection = $('.home');
+  const windowHeight = $(window).outerHeight();
+  if (homeSection.hasClass('home-fullscreen')) {
+    $('.home-fullscreen').css('height', windowHeight);
+  }
 }
 
-/* initialize */
-$(window).load(function () {
-    homeFullScreen();
-    smoothScroll.init();
+// initialize
+$(window).load(() => {
+  homeFullScreen();
+  smoothScroll.init();
 });
 
-/* on window resize */
-$(window).resize(function () {
-    homeFullScreen();
+// on window resize...
+$(window).resize(() => {
+  homeFullScreen();
 });
